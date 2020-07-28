ee230d732c1d3ee5052e7a97bb76c3c953456fb7
Alex <revolalex@gmail.com>
2020-07-28 16:34:35 +0200

    group get succes

--- server.js
+++ server.js
@@ -1,44 +1,49 @@
-const express = require('express');  //Imports the express module
-const app = express();  //Creates an instance of the express module
-const fetch = require ('node-fetch')
+const express = require("express"); //Imports the express module
+const app = express(); //Creates an instance of the express module
+const fetch = require("node-fetch");
 
 const PORT = 3000; //Randomly chosen port
 let studentArray = [];
+let groupsArray = [];
 
-let getStudentName = async ()=>{
-    let studentData = await fetch('http://localhost:8080/Students')
-    let stud = await studentData.json()
-    for (let index = 0; index < stud.length; index++) {
-       studentArray.push(stud[index].name) 
-    }
-}
-getStudentName()
+let getStudentName = async () => {
+  let studentData = await fetch("http://localhost:8080/Students");
+  let stud = await studentData.json();
+  for (let index = 0; index < stud.length; index++) {
+    studentArray.push(stud[index].name);
+  }
+};
+getStudentName();
 
+let getGroupsName = async () => {
+  let groupsData = await fetch("http://localhost:8080/Groups");
+  let group = await groupsData.json();
+  for (let index = 0; index < group.length; index++) {
+    groupsArray.push(group[index].name);
+  }
+};
+getGroupsName();
 
-app.set('view engine','ejs'); 
-
+app.set("view engine", "ejs");
 
 //Creates a Root Route
-app.get('/',function(req, res){
-    res.render('index.ejs');  //renders the index.jade file into html and returns as a response. The render function optionally takes the data to pass to the view.
+app.get("/", function (req, res) {
+  res.render("index.ejs"); //renders the index.jade file into html and returns as a response. The render function optionally takes the data to pass to the view.
 });
 
-app.get('/Students', async function(req, res){
-     res.render('students.ejs', {studentArray});  
+app.get("/Students", async function (req, res) {
+  res.render("students.ejs", { studentArray });
 });
 
-app.get('/Groups',function(req, res){
-    res.render('groups.ejs');  
+app.get("/Groups", async function (req, res) {
+  res.render("groups.ejs", { groupsArray });
 });
 
 //Starts the Express server with a callback
-app.listen(PORT, function(err) {
-    if (!err) {
-        console.log("http://localhost:3000/");
-    } else {
-        console.log(JSON.stringify(err));
-    }
+app.listen(PORT, function (err) {
+  if (!err) {
+    console.log("http://localhost:3000/");
+  } else {
+    console.log(JSON.stringify(err));
+  }
 });
-
-
-
